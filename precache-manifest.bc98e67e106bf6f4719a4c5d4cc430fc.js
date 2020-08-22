39fd21cbe52987fd114f2ef19add33c54c9a8b03
Patrick Easter <epatri6@gmail.com>
2020-08-21 21:27:33 -0400

    Updates

---
+++ precache-manifest.bc98e67e106bf6f4719a4c5d4cc430fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36bfdba2abb18a5b0bc67fc55b3acc1",
+    "url": "/ReactTests/index.html"
+  },
+  {
+    "revision": "1d9849e7c07a05e643c4",
+    "url": "/ReactTests/static/css/main.54909408.chunk.css"
+  },
+  {
+    "revision": "ea9cbefd60f750e1df5e",
+    "url": "/ReactTests/static/js/2.db85e3af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTests/static/js/2.db85e3af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9849e7c07a05e643c4",
+    "url": "/ReactTests/static/js/main.0ef97914.chunk.js"
+  },
+  {
+    "revision": "ba8d1eaeb73b4b49f9d2",
+    "url": "/ReactTests/static/js/runtime-main.55305c10.js"
+  },
+  {
+    "revision": "5c8f9265dcc6847704c8e80ec2071101",
+    "url": "/ReactTests/static/media/tile000.5c8f9265.png"
+  },
+  {
+    "revision": "97bc715596e3a77555ab0afbc87baa79",
+    "url": "/ReactTests/static/media/tile001.97bc7155.png"
+  },
+  {
+    "revision": "882c26ecbfd2f4fad2287f34c4112c0b",
+    "url": "/ReactTests/static/media/tile002.882c26ec.png"
+  },
+  {
+    "revision": "c2ded814560e9c5cab318dbb371e4962",
+    "url": "/ReactTests/static/media/tile003.c2ded814.png"
+  }
+]);
